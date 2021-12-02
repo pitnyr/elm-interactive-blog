7da6fd9d9e5f7c4bd321043ed6e8365cee2f81be
Pit Capitain <pit@capitain.de>
2021-12-02 20:39:51 +0100

    Add example for stateful-like markdown
    See https://pitnyr.github.io/elm-interactive-blog/making-of/setup.html#commit-2021-12-02-20-39

--- src/Book/FirstChapter.elm
+++ src/Book/FirstChapter.elm
@@ -1,13 +1,43 @@
 module Book.FirstChapter exposing (firstChapter)
 
+import Book.CounterChapter as CounterChapter
+import Book.InputChapter as InputChapter
 import ElmBook.Chapter as BC
 import Html as H
 
 
-firstChapter : BC.Chapter x
+type alias SharedState x =
+    { x
+        | inputModel : InputChapter.Model
+        , counterModel : CounterChapter.Model
+    }
+
+
+firstChapter : BC.Chapter (SharedState x)
 firstChapter =
     BC.chapter "The First Chapter"
-        |> BC.withComponent component
+        |> BC.withComponentList
+            [ ( "component", component ) ]
+        |> BC.withStatefulComponentList
+            [ ( "counterValue"
+              , \{ counterModel } ->
+                    H.text <|
+                        if counterModel > 0 then
+                            "Hey, you've found the Counter Chapter! The actual count is " ++ String.fromInt counterModel ++ "."
+
+                        else
+                            ""
+              )
+            , ( "inputValue"
+              , \{ inputModel } ->
+                    H.text <|
+                        if String.isEmpty inputModel.value then
+                            ""
+
+                        else
+                            "The value from the Input Chapter is \"" ++ inputModel.value ++ "\"."
+              )
+            ]
         |> BC.render content
 
 
@@ -23,7 +53,13 @@ content =
 
 Oh, look – A wild real component!
 
-<component />
+<component with-label="component" />
+
+Test
+
+ <component with-label="counterValue" with-hidden-label="true" with-display="inline" /><component with-label="inputValue" with-hidden-label="true" with-display="inline" />
+
+Noch irgendein Text, der sich vielleicht idealerweise gleich über mehrere (inbesondere mehr als eine) Zeilen erstreckt.
 
 Woof! Moving on...
 """
